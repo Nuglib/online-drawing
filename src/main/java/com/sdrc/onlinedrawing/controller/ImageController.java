bae38a6b803d1932155a5c3fd31425f310418ea9
m17521137221@163.com <m17521137221@163.com>
2019-10-19 11:29:24 +0800

    修改banner接口

--- src/main/java/com/sdrc/onlinedrawing/controller/ImageController.java
+++ src/main/java/com/sdrc/onlinedrawing/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.sdrc.onlinedrawing.controller;
 
+import com.sdrc.onlinedrawing.pojo.UserAdmin;
 import com.sdrc.onlinedrawing.service.ImageService;
 import com.sdrc.onlinedrawing.util.InfoUtils;
 import io.swagger.annotations.Api;
@@ -8,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 
@@ -44,5 +47,11 @@ public class ImageController {
     @ApiOperation(value = "获取风格元素")
     public InfoUtils getStyleElementsGroupBy(){
         return this.imageService.getStyleElementsGroup();
+    }
+
+    @RequestMapping("uploadbannerimage")
+    @ApiOperation("上传轮播图")
+    public InfoUtils uploadBannerImage(@RequestParam("image") MultipartFile[] image, UserAdmin userAdmin){
+        return null;
     }
 }
